9215c68747ffb3728c63a0420ebd6d88accae8f2
Dan Danciu <dan@ddanciu.ro>
2011-06-12 14:53:15 +0300

    RFC

--- finev-io/finev-io-api/src/main/java/ro/ddanciu/finev/io/Reader.java
+++ finev-io/finev-io-api/src/main/java/ro/ddanciu/finev/io/Reader.java
@@ -3,7 +3,6 @@ package ro.ddanciu.finev.io;
 import java.io.BufferedReader;
 import java.text.ParseException;
 
-import ro.ddanciu.finev.io.gmsh.GmshFileParseException;
 import ro.ddanciu.finite.elements.api.PoliLine;
 
 public interface Reader {
